ff07a66a95ad5cd5dfc10b229bdd713c163ade7c
Tim Riemer <tim.riemer@gmx.de>
2017-10-11 06:42:36 +0200

    add route and handler for sse

--- src/main/kotlin/mobi/riemer/KotlinbootApplication.kt
+++ src/main/kotlin/mobi/riemer/KotlinbootApplication.kt
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication.run
 import org.springframework.boot.autoconfigure.*
 import org.springframework.context.*
 import org.springframework.context.support.*
+import org.springframework.http.MediaType.APPLICATION_STREAM_JSON
 import org.springframework.http.MediaType.TEXT_HTML
 import org.springframework.web.reactive.function.server.*
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import reactor.core.publisher.*
+import java.time.*
 
 @SpringBootApplication
 class KotlinbootApplication : ApplicationContextInitializer<GenericApplicationContext> {
@@ -30,12 +32,20 @@ class Handler {
                     Mono.just("Hello, ${req.queryParam("name").orElse("Kotlin")}"),
                     String::class.java
             )
+
+    fun sse(req: ServerRequest): Mono<ServerResponse> =
+            ok().bodyToServerSentEvents(
+                    Flux.interval(Duration.ofSeconds(1)).map { "Hello $it" }
+            )
 }
 
 class Router(private val handler: Handler) {
     fun routes() = router {
         ("/" and accept(TEXT_HTML)).nest {
             GET("/hello", handler::hello)
+        }
+        ("/sse" and accept(APPLICATION_STREAM_JSON)).nest {
+            GET("/", handler::sse)
         }
     }
 }
